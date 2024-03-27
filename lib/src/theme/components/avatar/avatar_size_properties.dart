20abd68d5d8719af3b1dff11c9b2b5c841c75932
odeassis <odeassis@duck.com>
2024-03-27 08:29:43 -0300

    refactor: add @immutable annotation to BaconAvatarSizeProperties class

--- lib/src/theme/components/avatar/avatar_size_properties.dart
+++ lib/src/theme/components/avatar/avatar_size_properties.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+@immutable
 class BaconAvatarSizeProperties
     extends ThemeExtension<BaconAvatarSizeProperties>
     with DiagnosticableTreeMixin {
