1d894009a3c4f76f398ff34b21b892abe8cf7615
odeassis <odeassis@duck.com>
2024-03-29 19:48:40 -0300

    feat: add BaconDefaultPrimitiveFontSizes class to manage default font sizes and styles

---
+++ lib/src/theme/tokens/primitives/font/fonts.dart
@@ -0,0 +1,63 @@
+import 'package:bacon/src/theme/tokens/primitives/font/base.dart';
+import 'package:flutter/material.dart';
+
+class BaconDefaultPrimitiveFontSizes extends BaconBasePrimitiveFontSizes {
+  const BaconDefaultPrimitiveFontSizes({
+    required super.fontSize12,
+    required super.fontSize14,
+    required super.fontSize16,
+    required super.fontSize18,
+    required super.fontSize20,
+    required super.fontSize24,
+    required super.fontSize28,
+    required super.fontSize32,
+    required super.fontSize36,
+    required super.fontSize40,
+    required super.fontSize44,
+    required super.fontSize48,
+    required super.fontSize52,
+    required super.fontSize96,
+    required super.fontSize112,
+    required super.letterSpacingDefault,
+    required super.letterSpacingSpaced,
+    required super.letterSpacingTight,
+    required super.height120,
+    required super.height140,
+    required super.height170,
+    required super.fontWeightLight,
+    required super.fontWeightRegular,
+    required super.fontWeightMedium,
+    required super.fontWeightBold,
+    required super.fontWeightBlack,
+  });
+
+  static const BaconDefaultPrimitiveFontSizes font =
+      BaconDefaultPrimitiveFontSizes(
+    fontSize12: 12.0,
+    fontSize14: 14.0,
+    fontSize16: 16.0,
+    fontSize18: 18.0,
+    fontSize20: 20.0,
+    fontSize24: 24.0,
+    fontSize28: 28.0,
+    fontSize32: 32.0,
+    fontSize36: 36.0,
+    fontSize40: 40.0,
+    fontSize44: 44.0,
+    fontSize48: 48.0,
+    fontSize52: 52.0,
+    fontSize96: 96.0,
+    fontSize112: 112.0,
+    letterSpacingDefault: 0,
+    letterSpacingSpaced: 2.5,
+    letterSpacingTight: -1,
+    height120: 1.2,
+    height140: 1.4,
+    height170: 1.7,
+    fontWeightLight: FontWeight.w300,
+    fontWeightRegular: FontWeight.w400,
+    fontWeightMedium: FontWeight.w500,
+    fontWeightBold: FontWeight.w700,
+    fontWeightBlack: FontWeight.w900,
+  );
+}
